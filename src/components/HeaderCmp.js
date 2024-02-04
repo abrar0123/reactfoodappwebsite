56f556b61b5b113d016496b9599c2e60562da3df
abrar0786 <theibrar1000@gmail.com>
2024-02-05 03:38:04 +0500

    added setting pg

--- src/components/HeaderCmp.js
+++ src/components/HeaderCmp.js
@@ -3,13 +3,17 @@ import NavBar from "./NavBar";
 import SideBar from "./SideBar";
 import Product from "./Main/Product";
 import Pricing from "./Main/Pricing";
+import Sidebar2 from "./Sidebar2";
+import Setting from "./Main/Setting";
 
 const HeaderCmp = () => {
   return (
     <React.Fragment>
       <NavBar />
-      <Pricing />
+      <Setting/>
+      {/* <Pricing /> */}
       {/* <SideBar /> */}
+      {/* <Sidebsar2 /> */}
     </React.Fragment>
   );
 };
