f23a69d8e50c529aa568fa8ca0c2048059ef3514
abrar hussain <theibrar1000@gmail.com>
2024-03-21 08:41:00 -0700

    added pdf

--- README.md
+++ README.md
@@ -19,3 +19,17 @@ complete control over items and to set and positioned anywhere ] -->
 grid-row , grid-columns :
 
 <!-- placement of grid items within a grid containe -->
+
+Syntax: grid-column: <start-line> / <end-line>;
+start-line: Specifies the grid line where the grid item starts.
+end-line: Specifies the grid line where the grid item ends.
+Example Usage:
+grid-column: 1 / 3;: The grid item starts from the first grid line and ends at the third grid line, spanning two columns.
+grid-column: 3 / span 2;: The grid item starts from the third grid line and spans two columns.
+Special Values:
+auto: Automatically places the grid item based on the size of its contents.
+span <n>: Spans the grid item across <n> grid tracks.
+-1: Represents the last line of the grid.
+Example Usage:
+grid-row: auto;: The grid item will be automatically placed within the grid row based on its content size.
+grid-column: 1 / -1;: The grid item starts from the first column and spans across all columns until the last one.
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { Document, Page } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 
 export default function Home() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1); // Start with first page
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+  // -------------view pdf ============
+
   return (
     <div className="container mx-auto bg-cyan-300">
       <h1 className="text-3xl font-bold underline">Hello, Tailwind CSS 1!</h1>
@@ -11,7 +21,7 @@ export default function Home() {
         Click Me
       </button>
       <button class="dark:md:hover:bg-fuchsia-600 ...">Save changes</button>
-      <form>
+      {/* <form>
         <label class="block">
           <span class="block text-sm font-medium text-slate-700">Username</span>
           <input
@@ -27,30 +37,22 @@ export default function Home() {
     "
           />
         </label>
-      </form>
+      </form> */}
       {/* -------------------- */}
-      <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-        <div class="md:flex">
-          <div class="md:shrink-0">
-            {/* <img class="h-48 w-full object-cover md:h-full md:w-48" src="/img/building.jpg" alt="Modern building architecture"> */}
-          </div>
-          <div class="p-8">
-            <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-              Company retreats
-            </div>
-            <a
-              href="#"
-              class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
-            >
-              Incredible accommodation for your team
-            </a>
-            <p class="mt-2 text-slate-500">
-              Looking to take your team away on a retreat to enjoy awesome food
-              and take in some sunshine? We have a list of places to do just
-              that.
-            </p>
-          </div>
-        </div>
+
+      <p>starts</p>
+
+      <div>
+        <Document
+          file="path/to/your/document.pdf"
+          onLoadSuccess={onDocumentLoadSuccess}
+          options={{ workerSrc: "/pdf.worker.js" }}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
       </div>
     </div>
   );
